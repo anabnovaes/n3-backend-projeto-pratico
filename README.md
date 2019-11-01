7099cf87ecb64636e2ad156fee729f14c8bbc91d
Ana Paula Novaes <ana-paula1503@hotmail.com>
2019-11-01 20:41:21 -0300

    Incluido readme.md

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
-# n3-backend-projeto-pratico
-Primeiro projeto prático de API realizado no bootcamp de backend da reprograma
+# API de lista de tarefas
+
+## n3-backend-projeto-pratico
+Primeiro projeto prático de API realizado na 4ª semana do bootcamp de backend da reprograma
+
+## Objetivo projeto
+
+Exibir via API uma listagem de tarefas, com rotas para listagem por ID, nome colaborador e por data de início do projeto.
+
+## Tecnologias usadas
+
+Para desenvolver o projeto utilizei a tecnologia Node JS. O download do Node é feito neste [link](https://nodejs.org/en/). A instalação é feita pelo procedimento padrão de instalação (next => next => ok). Para o download dos pacotes utilizei o NPM, porém este pacote é opcional e poderá ser usado conforme desejado.
+
+## Instalação
+
+Após clonar o repositório você deverá iniciar o node pelo seguinte comando 
+
+```sh
+npm init 
+```
+
+## Endpoints utilizados
+
+Os endpoints criados foram os seguintes
+
+GET /:id => Exibe os dados de acordo com o ID inserido
+GET /  => Exibe todas as tarefas incluídas
+GET /concluidos/filtrar => Exibe apenas as tarefas concluídas
+GET /:nome/buscar => Exibe apenas os dados do nome filtrado
+GET /dataInclusao/filtrar => Ordena a exibição de acordo com as datas mais recentes
--- src/controller/tarefasController.js
+++ src/controller/tarefasController.js
@@ -1,5 +1,16 @@
 const tarefas = require('../model/lista-tarefas.json');
 
+
+  //função para transformar string em tipo date
+  function stringParaData(data){
+    const dataSplit = data.split("/");
+    const dataComSeparador = dataSplit[1] + '-' + dataSplit[0] + '-' +
+    dataSplit[2];   
+    const dataFormatada = new Date(dataComSeparador);
+    return dataFormatada;
+  }
+
+
 //obtendo a rota pelo id 
 exports.getId = (req, res) => {
     const id = req.params.id
@@ -36,29 +47,18 @@ exports.getId = (req, res) => {
 
 
   // pesquisando rota por data de exibição
-  exports.getOrdenadoPorData = (req, res)=>{
-   //função para transformar a string em data 
-  function stringParaData(data){
-    const dataSplit = data.split("/");
-    const dataComSeparador = dataSplit[1] + '-' + dataSplit[0] + '-' +
-    dataSplit[2];    
-    const dataFormatada = new Date(dataComSeparador).toLocaleDateString();
-    console.log(dataFormatada)
-    return dataFormatada
-    
-  }
-  
-    
+  exports.getOrdenadoPorData = (req, res)=>{    
  const datasOrdenadas = tarefas.sort(function (a, b) {
-      if (stringParaData(a.dataInclusao) > stringParaData(b.dataInclusao)) {
+      if (stringParaData(a.dataInclusao) < stringParaData(b.dataInclusao)) {
         return 1;
       }
-      if (stringParaData(a.dataInclusao) < stringParaData(b.dataInclusao)){
+      if (stringParaData(a.dataInclusao) > stringParaData(b.dataInclusao)){
         return -1;
       }
-      // a must be equal to b
       return 0;
     });
 
     res.send(datasOrdenadas)
   }
+
+  // incluindo tempo de conclusão da tarefa
