8b29e679bb8261a13fe3fb4d273f1b553f24d400
Ana Paula Novaes <ana-paula1503@hotmail.com>
2019-10-31 22:25:09 -0300

    Ajustada rota de ordenação alfabetica

--- src/controller/tarefasController.js
+++ src/controller/tarefasController.js
@@ -37,24 +37,23 @@ exports.getId = (req, res) => {
 
   // pesquisando rota por data de exibição
   exports.getOrdenadoPorData = (req, res)=>{
-   
-
-
-//   //função para transformar a string em data 
+   //função para transformar a string em data 
   function stringParaData(data){
     const dataSplit = data.split("/");
     const dataComSeparador = dataSplit[1] + '-' + dataSplit[0] + '-' +
     dataSplit[2];    
     const dataFormatada = new Date(dataComSeparador).toLocaleDateString();
+    console.log(dataFormatada)
     return dataFormatada
+    
   }
   
     
- const datasOrdenadas =  tarefas.sort(function (a, b) {
-      if (stringParaData(a.nomeColaborador) > stringParaData(b.nomeColaborador)) {
+ const datasOrdenadas = tarefas.sort(function (a, b) {
+      if (stringParaData(a.dataInclusao) > stringParaData(b.dataInclusao)) {
         return 1;
       }
-      if (stringParaData(a.nomeColaborador) < stringParaData(b.nomeColaborador)){
+      if (stringParaData(a.dataInclusao) < stringParaData(b.dataInclusao)){
         return -1;
       }
       // a must be equal to b
